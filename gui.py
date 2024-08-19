2c6307af8ce8172cc4259aad9a0bd5d310d3dcb0
AmenoAmena <ardacavusoglu666@gmail.com>
2024-08-19 17:02:32 +0300

    file name done now going to make headless dont know why but iT sometimes giver error while headless

--- gui.py
+++ gui.py
@@ -13,6 +13,9 @@ class Frontend(ctk.CTk):
         self.product_name = ctk.CTkEntry(self, placeholder_text="Product Name: ")
         self.product_name.pack(pady=20)
 
+        self.file_name = ctk.CTkEntry(self, placeholder_text="File name: ")
+        self.file_name.pack(pady=20)
+
         self.options = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10"]
         self.option_menu = ctk.CTkOptionMenu(self, values=self.options)
         self.option_menu.pack(pady=20)
@@ -24,13 +27,14 @@ class Frontend(ctk.CTk):
         
         self.product_name = self.product_name.get()
         self.page_number = self.option_menu.get()
+        self.file_name = self.file_name.get()
 
         self.scraping()        
 
     def scraping(self):
         self.scraper = Backend()
         self.scraper.scrape(self.product_name,self.page_number)
-        self.scraper.save_to_csv('products.csv')
+        self.scraper.save_to_csv(self.file_name)
         self.scraper.quit_driver()
 
 
